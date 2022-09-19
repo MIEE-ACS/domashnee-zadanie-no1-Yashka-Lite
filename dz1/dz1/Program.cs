6ba0db2306534a446434126bba9af807d7ee6bb5
Yashka-Lite <rosinskyzhenya@yandex.ru>
2022-09-19 22:48:13 +0300

    dz1

---
+++ dz1/dz1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace dz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите x:");
+            double x = double.Parse(Console.ReadLine().Replace(',', '.'), CultureInfo.InvariantCulture);
+            Console.WriteLine("Уравнение y = 4*(x-3)^6 -7*(x-3)^3 +2");
+            double y = 4 * Math.Pow(x - 3, 6) - 7 * Math.Pow(x - 3, 3) + 2;
+            Console.WriteLine($"y={y}");
+        }
+    }
+}
